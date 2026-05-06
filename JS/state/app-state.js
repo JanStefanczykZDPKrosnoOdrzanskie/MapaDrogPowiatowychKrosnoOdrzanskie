0eed808d7c801bead2639af5caa600885ddd0a4b
JanStefanczykZDPKrosnoOdrzanskie <j.stefanczyk@zdp.powiatkrosnienski.pl>
2026-05-06 13:14:57 +0200

    Anty-sabotage

--- JS/state/app-state.js
+++ JS/state/app-state.js
@@ -1,28 +1,3 @@
-/* ===============================
-APP STATE
-Centralny stan aplikacji
-=============================== */
-
-let ZDP_ALL_FEATURES = [];
-
-let CURRENT_BASEMAP = "osm";
-
-let KM_MEASURE_ENABLED = false;
-let KM_INTERMEDIATE_LOADED = false;
-let CURRENT_KM_MARKER = null;
-
-let PR_SELECTED_TYPES = {
-  MOTO: true,
-  SOMB: true,
-  LSMC: true,
-  SCBP: true,
-  SCZP: true,
-  ATBS: true,
-  CGNR: true
-};
-
-let PR_LAST_ROW = null;
 let PR_CSV_RAW = null;
-
 let PR_SPEED_HIST = {};
 let PR_TIME_HIST = {};
