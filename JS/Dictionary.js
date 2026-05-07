23ab59d86922b00079bd97e347803f40c56feb22
JanStefanczykZDPKrosnoOdrzanskie <j.stefanczyk@zdp.powiatkrosnienski.pl>
2026-05-07 07:38:40 +0200

    Create Dictionary.js

---
+++ JS/Dictionary.js
@@ -0,0 +1,20 @@
+const NAW_DICT = {
+  ASF: "Masa bitumiczna",
+  GRU: "Grunt naturalny",
+  KAM: "Kostka kamienna",
+  BRU: "Bruk",
+  TLU: "Tłuczeń",
+  ZWR: "Żwir",
+  PRF: "Kostka prefabrykowana"
+};
+const KL_DICT = {
+  G: "G - Główna",
+  Z: "Z - zbiorcza",
+  L: "L - lokalna"
+};
+const ZUD_DICT = {
+  2: "Standard 2",
+  3: "Standard 3",
+  4: "Standard 4",
+  6: "Standard 6"
+};
